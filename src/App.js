9a411ddaa05011faf03b603e9fb1974f8deee592
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-16 23:47:18 -0500

    Fullstack part2 section c exercises : the phonebook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Persons from "./Components/Persons";
+import PersonForm from "./Components/PersonForm";
+import Filter from "./Components/Filter";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [filteredByName, setFilteredByName] = useState([]);
+  const [filterStatus, setFilterStatus] = useState(false);
+
+  const nameAlreadyAdded = persons.some((person) => person.name === newName);
+  const personsToShow = filterStatus ? filteredByName : persons;
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    nameAlreadyAdded
+      ? window.alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat(newPerson));
+
+    setNewName("");
+    setNewNumber("");
+    setFilterStatus(false);
+    setFilterValue("");
+  };
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    // console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value);
+    const regexp = new RegExp(event.target.value, "i");
+    setFilteredByName(persons.filter((person) => regexp.test(person.name)));
+    event.target.value === "" ? setFilterStatus(false) : setFilterStatus(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter value={filterValue} onChange={handleFilterChange} />
+      <h2>Add a new</h2>
+      <PersonForm
+        onSubmit={addPerson}
+        nameValue={newName}
+        nameOnChange={handleNameChange}
+        numberValue={newNumber}
+        numberOnChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} />
     </div>
   );
-}
+};
 
 export default App;
