ba281e356b5b7be354c6f5205ac4d4db64f20ace
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-17 00:11:24 -0500

    Fullstack part2 section c exercises : 2.11 completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Persons from "./Components/Persons";
 import PersonForm from "./Components/PersonForm";
 import Filter from "./Components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterValue, setFilterValue] = useState("");
   const [filteredByName, setFilteredByName] = useState([]);
   const [filterStatus, setFilterStatus] = useState(false);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((res) => {
+      console.log("promise fulfilled");
+      setPersons(res.data);
+    });
+  }, []);
 
   const nameAlreadyAdded = persons.some((person) => person.name === newName);
   const personsToShow = filterStatus ? filteredByName : persons;
